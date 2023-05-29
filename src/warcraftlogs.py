ad112ea11f37c86261daa6198679dc7df7e80ec6
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2023-05-29 13:58:25 +0200

    feat: `/logs-from` -> logs from a specific date

--- src/warcraftlogs.py
+++ src/warcraftlogs.py
@@ -1,7 +1,7 @@
 import requests
 from src import log
 import os
-from datetime import datetime
+from datetime import datetime, date
 
 # Initialize logger
 logger = log.setup_logger(__name__)
@@ -55,6 +55,55 @@ async def latest_logs() -> str:
         return "> **ERROR: Something went wrong, please try again later!**"
 
 
-async def logs(date: datetime) -> str:
-    pass
-    return str()
+async def logs_from(date: date) -> str:
+    """Fetches the Warcraft logs for a particular date and returns the log URLs.
+
+    Args:
+        date (date): The date to fetch the logs for.
+
+    Returns:
+        str: URLs of the logs or an error message.
+    """
+    # Get WarcraftLogs API token from environment variables
+    WL_API_KEY = os.getenv("WARCRAFT_LOGS_API_KEY")
+
+    # Base URL for Warcraft logs API
+    base_guild_url = (
+        BASE_REQUEST_URL + "reports/guild/Casual%20Progress/eredar/eu?api_key="
+    )
+
+    # Fetch API key from environment variables
+    url = base_guild_url + str(WL_API_KEY)
+    data = requests.get(url)
+    urls = []
+    try:
+        if data.status_code == 200:
+            # Parse the response JSON
+            data_json = data.json()
+
+            for log in data_json:
+                # Fetch log ID
+                log_id = log["id"]
+
+                # Fetch log start timestamp
+                start_timestamp = log["start"]
+                start_datetime = datetime.fromtimestamp(start_timestamp / 1000)
+
+                # Check if the date matches
+                if start_datetime.date() == date:
+                    # Construct the log URL
+                    log_url = BASE_RESPONSE_REPORTS_URL + log_id
+                    urls.append(log_url)
+
+            return "\n".join(urls) if urls else "> **ERROR: No logs found for the given date!**"
+
+        else:
+            logger.warning(
+                f"\x1b[31mRequest failed with status code {data.status_code}\x1b[0m"
+            )
+            return "> **ERROR: Something went wrong, please try again later!**"
+
+    except Exception as e:
+        logger.exception(f"Error while sending message: {e}")
+        return "> **ERROR: Something went wrong, please try again later!**"
+
