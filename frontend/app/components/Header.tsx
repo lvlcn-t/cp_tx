e6e8f316f80a4bde1e78683c5ded67d1a5adaa62
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2023-05-30 19:53:32 +0200

    feat: add header component

---
+++ frontend/app/components/Header.tsx
@@ -0,0 +1,30 @@
+// components/Header.tsx
+import React from 'react';
+import Link from 'next/link';
+import SignInButton from './SignInButton'
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-600 text-white px-6 py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="/" className="text-2xl font-semibold">
+          Casual Progress
+        </a>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/settings" className="ml-4">
+                Settings
+              </Link>
+            </li>
+            <li>
+              <SignInButton />
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
